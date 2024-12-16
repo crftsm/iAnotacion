1bbb09fdc3898432d53378eaabbe635b6722604f
crftsm <185578355+crftsm@users.noreply.github.com>
2024-12-16 18:47:16 -0300

    Add files via upload

---
+++ readme.md
@@ -0,0 +1,14 @@
+# iAnotación
+
+Una sola página web que permite al usuario escribir su tareas y almazenar en el navegador de Internet.  
+Puedes acceder los datos almacenados siempre que quiera, ya que los datos son almacenaos en `localStorage`.
+
+## Sus caracteristicas
+
+Una de las caracteristicas de iAnotación es en la experiencia del usuario, toda la página es muy sencillo y cualquier persona puedes manejar con suceso la aplicación. También no es necesario entrar con una cuenta externa o cualquier otra cosa similar. La aplicación también es muy ligero, entoces no tendrás problemas con esto.
+
+### Tecnologías
+
+- HTML5
+- LESS/CSS
+- JavaScript Vanilla
---
+++ src/application.js
@@ -0,0 +1,77 @@
+"use strict";
+//=========================================================================
+if (localStorage.length > 0) {
+  document.getElementById("lista-tareas").innerHTML = "";
+  for (let i = 0; i < localStorage.length; i++) {
+    let clave = localStorage.key(i);
+    document.getElementById("lista-tareas").innerHTML +=
+      localStorage.getItem(clave);
+  }
+} else {
+  document.getElementById("lista-tareas").innerText =
+    "¡No hay ninguna tarea guardada en iAnotación!";
+  document.getElementById("lista-tareas").style.padding = "12px";
+}
+//=========================================================================
+let fechaFormateada = new Intl.DateTimeFormat("es-ES", {
+  weekday: "long",
+  year: "numeric",
+  month: "long",
+  day: "numeric",
+}).format(new Date());
+
+document.getElementById("fecha").innerText = fechaFormateada =
+  fechaFormateada.charAt(0).toUpperCase() + fechaFormateada.slice(1);
+//=========================================================================
+document.getElementById("anadir-tarea-btn").addEventListener("click", () => {
+  const tareaID = crypto.randomUUID();
+  let plantilla = `<li class="la-tarea" id="tarea-${tareaID}">${
+    document.getElementById("tarea").value
+  }</li>`;
+  localStorage.setItem(`tarea-${tareaID}`, plantilla);
+  window.location.reload();
+});
+//=========================================================================
+document.getElementById("borrar-todo").addEventListener("click", () => {
+  document.getElementById("lista-tareas").innerHTML = "";
+  localStorage.clear();
+  document.getElementById("lista-tareas").innerText =
+    "¡No hay ninguna tarea guardada en iAnotación!";
+  document.getElementById("lista-tareas").style.padding = "12px";
+});
+//=========================================================================
+if (document.querySelector(".la-tarea")) {
+  document.querySelectorAll(".la-tarea").forEach((tarea) => {
+    tarea.addEventListener("click", function () {
+      document
+        .getElementById("lista-tareas")
+        .removeChild(document.querySelector(".la-tarea"));
+      localStorage.removeItem(`${this.id}`);
+      window.location.reload();
+    });
+  });
+}
+//=========================================================================
+document.querySelectorAll(".la-tarea").forEach((tarea) => {
+  let timeoutId;
+
+  tarea.addEventListener("mouseover", function () {
+    timeoutId = setTimeout(function () {
+      const nuevaTarea = prompt("Edite o valor da tarefa:", tarea.textContent);
+      if (nuevaTarea && nuevaTarea.trim() !== "") {
+        localStorage.setItem(
+          tarea.id,
+          `<li class="la-tarea" id="${tarea.id}">${nuevaTarea}</li>`
+        );
+        tarea.textContent = nuevaTarea;
+      }
+    }, 1500);
+  });
+
+  tarea.addEventListener("mouseout", () => clearTimeout(timeoutId));
+  tarea.addEventListener("click", function () {
+    localStorage.removeItem(tarea.id);
+    tarea.remove();
+  });
+});
+//=========================================================================
